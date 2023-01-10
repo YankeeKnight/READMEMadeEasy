f8b3f43b5f9138d3cc4050f293a0068bad407fd6
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-01-10 10:32:39 -0500

    added code for generateMarkdown.js and index.js

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your git hub username?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your project about?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How will your project be used?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the License you are using",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "None"]
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Please add any collaborators or third party assets."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please advise how you would run a test for this project?"
+    },
+    {
+        type: "input",
+        name: "readmeTitle",
+        message: "Please name your README file, excluding the '.md'."
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+const writeToFile = (fileName, data) =>
+    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.log(err)
+            : console.log("Your README file was successfully generated!")
+    );
 
 // TODO: Create a function to initialize app
-function init() { }
+const init = () => {
+    inquirer.prompt(questions).then((answers) => {
+        writeToFile(`../${answers.readmeTitle}.md`, answers);
+    });
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) =>
+  license === "None"
+    ? ""
+    : `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (license) =>
+  license === "None"
+    ? ""
+    : `5. [License](#license)`;
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (license) =>
+  license === "None"
+    ? ""
+    : `## License
+
+Licensed under the ${license} license.`;
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+//title, description, table of contents, installation, usage, license, contributing, and tests
+const generateMarkdown = ({
+  name, title, description, installation, usage, credits, tests, license
+}) =>
+  `# ${title}
 
-`;
-}
+${renderLicenseBadge(license)}
+
+## Description
+${description}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Credits](#credits)
+4. [Tests](#tests)
+${renderLicenseLink(license)}
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Credits
+${credits}
+
+## Tests
+${tests}
+
+${renderLicenseSection(license)}
+
+Copyright © ${name}. All rights reserved.`;
 
 module.exports = generateMarkdown;
